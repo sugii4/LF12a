44903309bb2c348aaf6d4ecf829823d5bed1e94c
Sugii4 <cindy.hinze2001@gmail.com>
2022-10-28 00:09:09 +0200

    .

--- public/app.js
+++ public/app.js
@@ -1,21 +1,26 @@
-console.log('seite ist fertig geladen')
-
 let button = document.getElementById('berechnen')
-let sterne = document.getElementById('sterne')
-let muenzen = document.getElementById('muenzen')
+let input_sterne = document.getElementById('sterne')
+let input_muenzen = document.getElementById('muenzen')
 
 button.addEventListener('click', function(event) { //erst wenn der Button geklickt wurde, führe den Code in den Klammern aus
+    
+    // if () //hier felder validieren
+    
+    let sterne = input_sterne.value
+    let muenzen = input_muenzen.value
     console.log('button wurde geklickt')
-    console.log(sterne.value) //greife auf den Wert von "sterne" zu
-    post('/scoreboard') //übergebe an localhost/scoreboard?
+    const data = `sterne=${sterne}&muenzen=${muenzen}&popularitaet=test&territorien=1&ressourcen=4&bonus=1&fabrik=0&gesamt=4`
+    post('/scoreboard', data) //übergebe an localhost/scoreboard?
 })
 
-function post(url){
-    var xhr = new XMLHttpRequest();
-xhr.open('POST', url, true);
-xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-xhr.onload = function () {
-    console.log(this.responseText);
-};
-xhr.send()
+function post(url, data) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    /*xhr.onload = function () {
+        console.log(this.responseText);
+    };*/
+    xhr.send(data)
 }
+
+//todo: in index.html elemente bauen wo der highscore angezeigt wird
--- server.js
+++ server.js
@@ -1,27 +1,39 @@
 const express = require('express')
 const mysql = require('mysql')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
+const connection = mysql.createConnection({ //verbinde dich mit der Datenbank, 
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'scoreboard'
+});
+
 app.use(express.static('./public')) //führt alles im Ordner "public" auf "localhost" aus
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/scoreboard',(req,res)=>{ //wenn ich localhost/scoreboard in die URL des Browsers eingebe, führe den Code in den Klammern aus
-  console.log(req.body)
-  res.send('Moin')
+  let body = req.body
+  connection.query(`INSERT INTO scorelist (Sterne, Muenzen, Popularitaet, Territorien, Ressourcen, Bonus, Fabrik, Gesamt) VALUES (${body.sterne},${body.muenzen}, floor(rand()*100), floor(rand()*100), floor(rand()*100), floor(rand()*100), 0, floor(rand()*100))`,function(error, results, fields) {
+    if (error) {
+      res.json({success:false,message:error.toString()})
+    } else {
+      res.json({success:true})
+    }
+  })
 })
 
 app.get('/scoreboard', (req, res) => { //wenn ich localhost/scoreboard in die URL des Browsers eingebe, 
-  const connection = mysql.createConnection({ //verbinde dich mit der Datenbank, 
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'scoreboard'
+  connection.query("SELECT * FROM scorelist order by id desc limit 1;", function (error, results, fields) {//speicher die Daten dort ein und 
+    res.json(results[0])
   });
-  connection.query("INSERT INTO `scorelist` (`id`, `Sterne`, `Muenzen`, `Popularitaet`, `Territorien`, `Ressourcen`, `Bonus`, `Fabrik`, `Gesamt`) VALUES (NULL, '5', '15', '8', '3', '2', '4', '0', '16')", function (error, results, fields) {//speicher die Daten dort ein und 
-    
-  connection.end(); //beende die Verbindung wieder
-  });
-  res.send('Hello World! asdasd')
 })
+
+//mysql heartbeat
+setInterval(function () {
+  connection.query('SELECT 1');
+  }, 5000);
 
 app.listen(port) //lass den Express-Server auf den definierten Port laufen
