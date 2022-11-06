93a18162e325636090db00bd952a3cf6e96c06b7
sugii <cindy.hinze2001@gmail.com>
2022-11-06 17:43:15 +0100

    es geht voran

--- public/app.js
+++ public/app.js
@@ -1,26 +1,29 @@
-let button = document.getElementById('berechnen')
-let input_muenzen = document.getElementById('muenzen')
-let input_popularitaet = document.getElementById('popularitaet')
-let input_ressourcen = document.getElementById('ressourcen')
-let input_territorien = document.getElementById('territorien')
-let input_fabrik = document.getElementById('fabrik')
-let input_sterne = document.getElementById('sterne')
-let input_bonus = document.getElementById('bonus')
-let input_gesamt = document.getElementById('gesamt')
+//let button = document.getElementById('berechnen')
+//let input_muenzen = document.getElementById('muenzen')
+//let input_popularitaet = document.getElementById('popularitaet')
+//let input_ressourcen = document.getElementById('ressourcen')
+//let input_territorien = document.getElementById('territorien')
+//let input_fabrik = document.getElementById('fabrik')
+//let input_sterne = document.getElementById('sterne')
+//let input_bonus = document.getElementById('bonus')
+//let input_gesamt = document.getElementById('gesamt')
 
+let form = document.getElementById('eingaben')
 
-button.addEventListener('click', function(event) { //erst wenn der Button geklickt wurde, führe den Code in den Klammern aus
-    
-    // if () //hier felder validieren
-    
-    let muenzen = input_muenzen.value
-    let popularitaet = input_popularitaet.value
-    let ressourcen = input_ressourcen.value
-    let territorien = input_territorien.value
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); //stoppt Weiterleitung zur scoreboard.php, führt stattdessen folgendes Script aus
+    console.log('form wurde gestoppt')
+
+    let data = new FormData(form) //speichert die eingegebenen Daten der inputs
+
+    //let muenzen = input_muenzen.value
+    //let popularitaet = input_popularitaet.value
+    //let ressourcen = input_ressourcen.value
+    //let territorien = input_territorien.value
     let fabrik = input_fabrik.value
-    let sterne = input_sterne.value
-    let bonus = input_bonus.value
-    let gesamt = input_gesamt.value
+    //let sterne = input_sterne.value
+    //let bonus = input_bonus.value
+    //let gesamt = input_gesamt.value
 
     if (fabrik === 'true') {
         fabrik = 1
@@ -28,19 +31,34 @@ button.addEventListener('click', function(event) { //erst wenn der Button geklic
         fabrik = 0
     }
 
-    console.log('button wurde geklickt')
-    const data = `sterne=${sterne}&muenzen=${muenzen}&popularitaet=${popularitaet}&territorien=${territorien}&ressourcen=${ressourcen}&bonus=${bonus}&fabrik=${fabrik}&gesamt=${gesamt}`
-    post('/scoreboard', data) //übergebe an localhost/scoreboard?
+    data.set('fabrik', fabrik) //aktualisiert die Fabrik in der FormData durch die if
+
+    fetch(form.action, { //übernimmt die Post Methode der form und nutzt dafür die scoreboard.php
+        method: form.method,
+        body: data //die daten aus den inputs
+    }).then(function (response) { //muss so
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(function (data) { //zeigt daten in der konsole an
+        console.log(data);
+    })
 })
 
-function post(url, data) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    /*xhr.onload = function () {
-        console.log(this.responseText);
-    };*/
-    xhr.send(data)
-}
+
+fetch('scoreboard.php', { //übernimmt die Get Methode und nutzt dafür die scoreboard.php
+    method: 'GET'
+}).then(function (response) { //muss so
+    if (response.ok) {
+        return response.json();
+    }
+    return Promise.reject(response);
+}).then(function (response) { //kann die abgefragten daten nun nutzen und anzeigen
+    console.log(response);
+    let ergebnis = response.data
+    console.log(ergebnis.Fabrik)
+    //alert(reponse.data.Muenzen) hier kannst du mit den werten aus der datenbank arbeiten
+})
 
 //todo: in index.html elemente bauen wo der highscore angezeigt wird
---
+++ scoreboard.php
@@ -0,0 +1,35 @@
+<?php
+
+require('connector.php'); //nutze den connector zum verbinden mit der datenbank
+mysqli_report(MYSQLI_REPORT_ERROR); //unterdrücke den fatal-error
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { //abfarge für post-methode
+    $muenzen = $_POST['muenzen'];
+    $popularitaet = $_POST['popularitaet'];
+    $ressourcen = $_POST['ressourcen'];
+    $territorien = $_POST['territorien'];
+    $fabrik = $_POST['fabrik'];
+    $sterne = $_POST['sterne'];
+    $bonus = $_POST['bonus'];
+    
+    $insert_daten = "INSERT INTO scorelist (Muenzen, Popularitaet, Ressourcen, Territorien, Fabrik, Sterne, Bonus) VALUES ('{$muenzen}', '{$popularitaet}', '{$ressourcen}', '{$territorien}', '{$fabrik}', '{$sterne}', '{$bonus}')";
+    
+    if (@mysqli_query($conn, $insert_daten)) { //fehleranalyse fürs einspeichern der daten
+        $response = ['success' => true];
+    } else {
+        $response = ['success' => false, 'message' => mysqli_error($conn)];
+    }
+    echo json_encode($response);
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET') { //abfrage für get-methode
+    $select_daten = "SELECT * FROM scorelist where id = 1"; //todo: richtige query bauen
+    $result = @mysqli_query($conn, $select_daten);
+    if (mysqli_error($conn)) { //fehleranalyse bei der connection
+        $response = ['success' => false, 'message' => mysqli_error($conn)]; //wenn connection fehlgeschlagen, hau fehlermeldung raus
+    } else { //ansonsten...
+        $data = mysqli_fetch_all($result,MYSQLI_ASSOC)[0]; //...nimm alle abgefragten daten, aber nur die erste zeile
+        $response = ['success' => true,'data'=>$data]; // und speicher die abgefragten daten in "data"
+    }
+    echo json_encode($response); //zeigs mir für mich leserlich
+}
+
+mysqli_close($conn);
